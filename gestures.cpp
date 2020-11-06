3e4779763d6b3d580b621a07e89fcc14a5f64733
Muellllkreis <rietig.m@googlemail.com>
2020-11-06 15:55:59 +0100

    test for cross-platform

--- gestures.cpp
+++ gestures.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
@@ -11,8 +12,17 @@ using namespace cv;
 using namespace std;
 
 
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+     if  (event == EVENT_LBUTTONDOWN) {
+          cout << "Position (" << x << ", " << y << ")" << endl;
+     }
+}
+
 int main(int argc, char* argv[])
 {
+    // Section to include webcam feed for later
+    /*
     VideoCapture cap;
     if(!cap.open(0))
         return 0;
@@ -26,5 +36,23 @@ int main(int argc, char* argv[])
           imshow("Feed", frame);
           if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
     }
+    */
+
+    vector<Rect> roi;
+
+    Mat I = imread("hand.jpg");
+    roi.push_back(Rect(645,150,30,30));
+    roi.push_back(Rect(615,244,30,30));
+    roi.push_back(Rect(725,262,30,30));
+    roi.push_back(Rect(623,333,30,30));
+    roi.push_back(Rect(690,333,30,30));
+    for(Rect r : roi) {
+        rectangle(I, r, Scalar(0, 255, 0));
+    }
+
+    imshow("Image", I);
+    setMouseCallback("Image", CallBackFunc, NULL);
+    waitKey(0);
+
     return 0;
 }
